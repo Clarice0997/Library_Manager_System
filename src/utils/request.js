598a6f32e9324346363dc00a0d58e021f1d2fce5
Clarice0997 <clarice09975@gmail.com>
2022-12-08 13:44:53 +0800

    feat: 二次封装axios网络请求 设置超时时间、拦截器、进度条

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,12 @@
 // 引入axios模块
 import axios from 'axios'
+
 // 引入获取token函数
 import { getToken } from '@/utils/auth'
+
+// 导入加载条
+import NProgress from 'nprogress'
+
 // 默认设置校验码
 axios.defaults.headers.common['Authorization'] = getToken()
 // 允许axios携带cookie
@@ -9,8 +14,31 @@ axios.defaults.headers.common['Authorization'] = getToken()
 
 // 封装axios
 const request = axios.create({
-  baseURL: process.env.VUE_APP_REQUEST_URL
+  baseURL: process.env.VUE_APP_REQUEST_URL,
+  // 设置超时时间
+  timeout: 5000
 })
+
+// 请求拦截器
+request.interceptors.request.use(config => {
+  // 开启进度条
+  NProgress.start()
+  return config
+})
+
+// 响应拦截器
+request.interceptors.response.use(
+  response => {
+    // 关闭进度条
+    NProgress.done()
+    return response
+  },
+  error => {
+    // 关闭进度条
+    NProgress.done()
+    return Promise.reject(error)
+  }
+)
 
 // 导出封装axios对象
 export default request
